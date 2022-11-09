06a25327398f604dfc378f9fd7485bfdef8842e6
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-08 23:39:30 -0300

    refactoring

--- client-server/public/controllers/UserController.js
+++ client-server/public/controllers/UserController.js
@@ -202,8 +202,8 @@ class UserController {
     }
 
     selectAll() {
-
-        HttpRequest.get('/users').then(data => {
+      
+        User.getUsersStorage().then(data => {
 
             data.users.forEach(dataUser => {
 
